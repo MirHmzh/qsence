748e63bb49bc29883e56f135825974d9e2aa3040
MirHmzh <corn.row81@gmail.com>
2020-10-15 10:27:01 +0700

    update socket cert

--- socket/app.js
+++ socket/app.js
@@ -1,5 +1,11 @@
+var fs = require( 'fs' );
 var app = require('express')();
-var http = require('http').createServer(app);
+var http = require('https').createServer({
+    key: fs.readFileSync('./cert/privkey.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+    requestCert: false,
+    rejectUnauthorized: false
+},app);
 var io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
